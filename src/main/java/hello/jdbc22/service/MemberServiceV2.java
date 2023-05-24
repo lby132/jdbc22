3cd0058e80b57f96806239f8f8a38c1a255c387c
lby132 <quddbscjswow@naver.com>
2023-05-25 00:35:32 +0900

    datasource

---
+++ src/main/java/hello/jdbc22/service/MemberServiceV2.java
@@ -0,0 +1,68 @@
+package hello.jdbc22.service;
+
+import hello.jdbc22.domain.Member;
+import hello.jdbc22.repository.MemberRepositoryV1;
+import hello.jdbc22.repository.MemberRepositoryV2;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 - 파라미터 연동, 풀을 고려한 종료
+ */
+@Slf4j
+@RequiredArgsConstructor
+public class MemberServiceV2 {
+
+    private final DataSource dataSource;
+    private final MemberRepositoryV2 memberRepository;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        Connection con = dataSource.getConnection();
+        try {
+            con.setAutoCommit(false);
+
+            //비즈니스 로직
+            bizLogic(con, fromId, toId, money);
+            con.commit();
+
+        } catch (Exception e) {
+            con.rollback(); //실패시 롤백
+            throw new IllegalStateException(e);
+        } finally {
+            release(con);
+        }
+
+
+
+    }
+
+    private void bizLogic(Connection con, String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(con, fromId);
+        Member toMember = memberRepository.findById(con, toId);
+
+        memberRepository.update(con, fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(con, toId, toMember.getMoney() + money);
+    }
+
+    private static void release(Connection con) {
+        if (con != null) {
+            try {
+                con.setAutoCommit(true); // 위에서 flase로 주었기 때문에 반납할때는 true로 주어야 커넥션 풀에 오토커밋이 true인상태로 유지
+                con.close();
+            } catch (Exception e) {
+                log.info("error", e);
+            }
+        }
+    }
+
+    private static void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+}
