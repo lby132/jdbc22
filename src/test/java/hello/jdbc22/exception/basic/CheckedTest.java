205f7c6f92a8b318e024886a0d995b879410319c
pro <quddbscjswow@naver.com>
2023-05-28 22:10:42 +0900

    exception

---
+++ src/test/java/hello/jdbc22/exception/basic/CheckedTest.java
@@ -0,0 +1,63 @@
+package hello.jdbc22.exception.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class CheckedTest {
+
+    @Test
+    void checked_catch() {
+        Service service = new Service();
+        service.callCatch();
+    }
+
+    @Test
+    void checked_throw() {
+        System.out.println("CheckedTest.checked_throw");
+    }
+
+
+    /**
+     * Exception을 상속받은 얘외는 체크 예외가 된다.
+     */
+    static class MyCheckedException extends Exception {
+        public MyCheckedException(String message) {
+            super(message);
+        }
+    }
+
+    /**
+     * Checked 예외는
+     * 예외를 잡아서 처리하거나 던지거나 둘중 하나를 필수로 선택해야한다.
+     */
+    static class Service {
+        Repository repository = new Repository();
+
+        /**
+         * 예외를 잡앟서 처리하는 코드
+         */
+        public void callCatch() {
+            try {
+                repository.call();
+            } catch (MyCheckedException e) {
+                log.info("예외처리, message={}", e.getMessage(), e);
+            }
+        }
+
+
+        /**
+         * 체크 얘외를 밖으로 던지는 코드
+         * 체크 예외는 예외를 잡지 않고 밖으로 던지려면 throw 예외를 메서드에 필수로 선언해야함
+          */
+        public void callThrow() throws MyCheckedException {
+            repository.call();
+        }
+    }
+
+    static class Repository {
+        public void call() throws MyCheckedException {
+            throw new MyCheckedException("ex");
+        }
+    }
+}
